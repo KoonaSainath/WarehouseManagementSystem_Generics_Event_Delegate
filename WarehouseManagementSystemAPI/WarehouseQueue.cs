6a26c3b5a55c240817cdd326991a9abec0c7f12f
= <=>
2024-03-29 20:55:49 +0530

    Delegate and event declaration

--- WarehouseManagementSystemAPI/WarehouseQueue.cs
+++ WarehouseManagementSystemAPI/WarehouseQueue.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WarehouseManagementSystemAPI
 {
+    //This delegate is used as event type. Basically we are creating a custom event using delegate concept.
+    public delegate void QueueChangedDelegate<T, U>(T sender, U eventData);
     /*
      * The class WarehouseQueue is used by console application to add/get units from the actual queue "warehouseQueue".
      * T is the type parameter, which acts as a placeholder for whatever type of data we want our queue to hold for a particular instance created.
@@ -14,6 +17,8 @@ namespace WarehouseManagementSystemAPI
     {
         private Queue<T> warehouseQueue = null;
 
+        //Event declaration using delegate we declared outside this class.
+        public event QueueChangedDelegate<WarehouseQueue<T>, EventData> eventQueueChanged;
         public WarehouseQueue()
         {
             this.warehouseQueue = new Queue<T>();
@@ -26,6 +31,8 @@ namespace WarehouseManagementSystemAPI
         public void AddUnit(T unit)
         {
             this.warehouseQueue.Enqueue(unit);
+
+            //Fire the event to print current warehouse unit message (entered the warehouse) and overall table of units as queue content has changed
         }
 
         /*
@@ -35,7 +42,17 @@ namespace WarehouseManagementSystemAPI
         public T GetNextUnit()
         {
             T nextUnit = this.warehouseQueue.Dequeue();
+
+            //Fire the event to print current warehouse unit message (placed in warehouse) and overall table of units as queue content has changed.
             return nextUnit;
         }
+    }
+
+    /*
+     * Inheriting the C# provided EventArgs class to be used as communication object between sender and receiver of events
+     */
+    public class EventData : EventArgs
+    {
+        public string Message { get; set; }
     }
 }
