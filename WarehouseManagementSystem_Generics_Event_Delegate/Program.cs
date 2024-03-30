aff894622fb23690168de3b3a3f4f2b304ea8dbb
= <=>
2024-03-30 20:42:17 +0530

    Implemented event handling logic to display current warehouse unit entered and status of all currently waiting units

--- WarehouseManagementSystem_Generics_Event_Delegate/Program.cs
+++ WarehouseManagementSystem_Generics_Event_Delegate/Program.cs
@@ -35,20 +35,38 @@ public class Program
     private static void WarehouseQueue_eventQueueChanged(WarehouseQueue<WarehouseUnit> sender, EventData eventData)
     {
         Console.Clear();
+        StatusHeading();
+        Console.WriteLine(eventData.Message);
+        IEnumerator<WarehouseUnit> warehouseUnits = sender.WarehouseQueueEnumerator;
+        if (!warehouseUnits.MoveNext())
+        {
+            Console.WriteLine();
+            Console.WriteLine("All units are placed in the warehouse");
+        }
+        else
+        {
+            TableHeading();
+            while (warehouseUnits.MoveNext())
+            {
+                WarehouseUnit warehouseUnit = warehouseUnits.Current;
+                Console.WriteLine($"{warehouseUnit.Id}{string.Empty.PadRight(20)}{warehouseUnit.Name}{string.Empty.PadRight(20)}{warehouseUnit.Type}{string.Empty.PadRight(20)}{warehouseUnit.Quantity}{string.Empty.PadRight(20)}{warehouseUnit.Price}{string.Empty.PadRight(20)}");
+            }
+        }
     }
 
     //Creates various types of instances of warehouse units and adds them to main list
     internal static void GetWarehouseUnits(List<WarehouseUnit> warehouseUnits)
     {
-        WarehouseUnit motor1 = GetWarehouseUnitInstance(Unit.Motor, 1, "Motor1", "Motor", 10, 200);
-        WarehouseUnit motor2 = GetWarehouseUnitInstance(Unit.Motor, 1, "Motor2", "Motor", 20, 300);
-        WarehouseUnit motor3 = GetWarehouseUnitInstance(Unit.Motor, 1, "Motor3", "Motor", 30, 400);
-        WarehouseUnit hammer1 = GetWarehouseUnitInstance(Unit.Motor, 1, "Hammer1", "Hammer", 20, 200);
-        WarehouseUnit hammer2 = GetWarehouseUnitInstance(Unit.Motor, 1, "Hammer2", "Hammer", 30, 300);
-        WarehouseUnit hammer3 = GetWarehouseUnitInstance(Unit.Motor, 1, "Hammer3", "Hammer", 40, 400);
-        WarehouseUnit weldingMachine1 = GetWarehouseUnitInstance(Unit.Motor, 1, "WeldingMachine1", "WeldingMachine", 10, 200);
-        WarehouseUnit weldingMachine2 = GetWarehouseUnitInstance(Unit.Motor, 1, "WeldingMachine2", "WeldingMachine", 15, 250);
-        WarehouseUnit weldingMachine3 = GetWarehouseUnitInstance(Unit.Motor, 1, "WeldingMachine3", "WeldingMachine", 20, 300);
+        int id = 0;
+        WarehouseUnit motor1 = GetWarehouseUnitInstance(Unit.Motor, ++id, "Motor1", "Motor", 10, 200);
+        WarehouseUnit motor2 = GetWarehouseUnitInstance(Unit.Motor, ++id, "Motor2", "Motor", 20, 300);
+        WarehouseUnit motor3 = GetWarehouseUnitInstance(Unit.Motor, ++id, "Motor3", "Motor", 30, 400);
+        WarehouseUnit hammer1 = GetWarehouseUnitInstance(Unit.Motor, ++id, "Hammer1", "Hammer", 20, 200);
+        WarehouseUnit hammer2 = GetWarehouseUnitInstance(Unit.Motor, ++id, "Hammer2", "Hammer", 30, 300);
+        WarehouseUnit hammer3 = GetWarehouseUnitInstance(Unit.Motor, ++id, "Hammer3", "Hammer", 40, 400);
+        WarehouseUnit weldingMachine1 = GetWarehouseUnitInstance(Unit.Motor, ++id, "WeldingMachine1", "WeldingMachine", 10, 200);
+        WarehouseUnit weldingMachine2 = GetWarehouseUnitInstance(Unit.Motor, ++id, "WeldingMachine2", "WeldingMachine", 15, 250);
+        WarehouseUnit weldingMachine3 = GetWarehouseUnitInstance(Unit.Motor, ++id, "WeldingMachine3", "WeldingMachine", 20, 300);
 
         warehouseUnits.Add(motor1);
         warehouseUnits.Add(motor2);
@@ -89,5 +107,26 @@ public class Program
             warehouseUnit.Price = price;
         }
         return warehouseUnit;
+    }
+
+    internal static void StatusHeading()
+    {
+        string statusHeading = "Current warehouse unit status";
+        Console.WriteLine();
+        Console.WriteLine(statusHeading);
+        Console.WriteLine(new string('-',statusHeading.Length));
+        Console.WriteLine();
+    }
+
+    internal static void TableHeading()
+    {
+        Console.WriteLine();
+        string tableHeading = "All units";
+        string columnsHeading = $"Id{string.Empty.PadRight(20)}Name{string.Empty.PadRight(20)}Type{string.Empty.PadRight(20)}Quantity{string.Empty.PadRight(20)}Price{string.Empty.PadRight(20)}";
+        Console.WriteLine(tableHeading);
+        Console.WriteLine(new string('-', tableHeading.Length));
+        Console.WriteLine(columnsHeading);
+        Console.WriteLine(new string('-', columnsHeading.Length));
+        Console.WriteLine();
     }
 }
